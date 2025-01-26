037ad25cf8318b95434457bff967f53112cef58c
victorpaul579 <victorpaul579@gmail.com>
2025-01-26 15:18:39 +0530

    Background colours changed

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -18,14 +18,14 @@ function Features() {
   ];
 
   return (
-    <section className="py-12 bg-gray-100">
+    <section className="py-12 bg-white">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl font-bold mb-6">Key Features</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {features.map((feature, index) => (
             <div
               key={index}
-              className="bg-white shadow-md rounded-lg p-6 flex flex-col items-center text-center hover:shadow-lg transition-shadow"
+              className=" bg-gray-100 shadow-md rounded-lg p-6 flex flex-col items-center text-center hover:shadow-lg transition-shadow"
             >
               <img
                 src={feature.image}
