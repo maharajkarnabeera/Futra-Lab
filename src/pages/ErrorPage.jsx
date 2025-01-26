0b3b3d9c1a0cd08437344b0aa9d1cd3c6ecb7b88
Maharaj Karna Beera <maharajkarnabeera24@gmail.com>
2025-01-26 15:26:22 +0530

    Error page added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,26 @@
-function ErrorPage() {
-  return (
-    <div>Error</div>
-  )
-}
+import { Link } from "react-router-dom";
 
-export default ErrorPage
+const ErrorPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 px-4">
+      <div className="text-center">
+        <h1 className="text-9xl font-extrabold text-gray-500">404</h1>
+        <h2 className="text-2xl md:text-4xl font-semibold mt-4">
+          Oops! Page not found.
+        </h2>
+        <p className="mt-2 text-gray-600">
+          The page you&#39;re looking for does&#39;nt exist or has been moved.
+        </p>
+
+        <Link
+          to="/"
+          className="mt-6 inline-block px-6 py-2 text-white bg-teal-500 rounded hover:bg-teal-600"
+        >
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
